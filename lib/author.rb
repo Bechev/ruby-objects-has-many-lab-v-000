970bdbae2eef42d43f006908a113eeac90039907
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-12 01:52:44 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,7 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     post.title = title
-    #post.author = self
+    post.author = self
     @posts << post
     @@post_count +=1
   end
